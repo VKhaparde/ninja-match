608c368b37a17a290e96c9593b0fb501a85cba56
VKhaparde <vandana.khaparde@gmail.com>
2019-10-04 14:44:16 -0700

    Finished Win Condition Feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,20 +2,18 @@ $(document).ready(initializeApp);
 var firstCardClicked = null;
 var secondCardClicked = null;
 var matches = null;
+var max_matches = 2;
 
 function initializeApp(){
 $('.card').on("click",'.card_back',handleCardClick);
 }
 function handleCardClick(event){
-
   console.log(event);
   console.log(event.currentTarget);
   $(event.currentTarget).addClass('hidden');
 
   if(firstCardClicked === null){
     firstCardClicked = $(event.currentTarget);
-
-    console.log(firstCardImage);
   }
   else{
     secondCardClicked = $(event.currentTarget);
@@ -28,18 +26,25 @@ function handleCardClick(event){
       matches = matches + 1;
       firstCardClicked = null;
       secondCardClicked = null;
-    } else {
+    }
+    else {
       //use setTimeout to call a function
         //in that function, remove the class hidden
         //set both first and second cardClicked to null
-
         setTimeout(function(){
           firstCardClicked.removeClass("hidden");
           secondCardClicked.removeClass("hidden");
           firstCardClicked = null;
           secondCardClicked = null;
-        },1500  );
-
+        },1500);
+    }
+    if (matches === max_matches) {
+      $('.modal').removeClass("hidden");
+      $('.modal_close').removeClass("hidden");
+      $('.modal_close').on("click",function(){
+        $('.modal').addClass("hidden");
+      });
     }
   }
+
 }
